e684de4ad6575a3971c8ed6d0e4afb9085a7b52a
DaJuan Harris <dajuancancode@gmail.com>
2019-01-19 12:49:08 -0500

    Update static side to include a modal that will appear when a button is clicked

--- static/src/components/Landing.js
+++ static/src/components/Landing.js
@@ -2,14 +2,36 @@ const React = require('react');
 
 require('./Landing.css')
 
+var Modal = require('./Modal')
+
 class Landing extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {show: false};
+    this.showModal = this.showModal.bind(this)
+    this.hideModal = this.hideModal.bind(this)
+  }
+
+  showModal() {
+    this.setState({show: true});
+  };
+
+  hideModal() {
+    this.setState({show: false})
+  }
+
   render() {
     return (
       <header className="main-header">
         <div className="header-content">
           <h1>Aux are us</h1>
           <p className='tagline'>Something catchy goes here</p>
-          <button className='btn landing-btn'>Let's get started</button>
+          <Modal show={this.state.show} handleClose={this.hideModal}>
+            <p>Modal</p>
+            <p>Data</p>
+          </Modal>
+          <button type="button" className='btn landing-btn' onClick={this.showModal}>Let's get started</button>
         </div>
       </header>
     )
