51f2a687ca08e7be9423d889f48a5662c4704580
adrian silva <adriansilva@adrians-MacBook-Air.local>
2019-01-19 20:53:29 -0500

    Sending JSON object from api endpoint

--- spotifyservice/main.go
+++ spotifyservice/main.go
@@ -28,11 +28,11 @@ var (
 var accseToke *string
 
 type Reponse struct {
-	access_token  string `json:"access_token"`
-	token_type    string `json:"token_type"`
-	scope         string `json:"scope"`
-	expires_in    int    `json:"expires_in"`
-	refresh_token string `json:"refresh_token"`
+	Accesstoken  string `json:"access_token"`
+	Token_type   string `json:"token_type"`
+	Scope        string `json:"scope"`
+	Expiresin    int    `json:"expires_in"`
+	Refreshtoken string `json:"refresh_token"`
 }
 
 type ObjectKey struct {
@@ -80,21 +80,30 @@ func callback(w http.ResponseWriter, r *http.Request) {
 	newStrenc := strings.Replace(strEnc, "=", "", -1)
 	fmt.Println(newStrenc)
 	request.Header.Set("Authorization", "Basic "+newStrenc)
+	request.Header.Set("Accept", " application/json")
 	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	//Authorization: Basic *<base64 encoded client_id:client_secret>*
 	client := http.Client{}
 	res, err := client.Do(request)
+	responseObject := Reponse{}
 	if err != nil {
 		panic("token was not obtained")
 	}
-	res.Header.Get("200")
 	defer res.Body.Close()
-	data, _ := ioutil.ReadAll(res.Body)
-	dt := string(data)
-	fmt.Println(dt)
-	// Object2 := &Reponse{}
-	// err = json.Unmarshal(data, Object2)
-	// fmt.Println(Object2.access_token)
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	d := string(data)
+	fmt.Println(d)
+	err = json.Unmarshal([]byte(d), &responseObject)
+	if err != nil {
+		fmt.Println("Error decoding json")
+	}
+	err = json.NewEncoder(w).Encode(responseObject)
+	if err != nil {
+		fmt.Println("Error decoding json", err)
+	}
 }
 
 func handlerObject(w http.ResponseWriter, r *http.Request) {
@@ -103,6 +112,6 @@ func handlerObject(w http.ResponseWriter, r *http.Request) {
 	}
 	err := json.NewEncoder(w).Encode(&object)
 	if err != nil {
-		fmt.Println("An error happened")
+		fmt.Println("An error happened", err)
 	}
 }
