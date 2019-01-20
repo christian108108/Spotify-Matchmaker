223ba987c0135b480d4cbf594464b90476b317ff
Christian Campana-Emard <christian.campana.emard@outlook.com>
2019-01-19 19:45:07 -0500

    small fix for top artists model

--- Sandbox/Sandbox.Library/Models/TopArtistsModel.cs
+++ Sandbox/Sandbox.Library/Models/TopArtistsModel.cs
@@ -6,6 +6,6 @@ namespace Sandbox.Library.Models
         public String Id { get; set; }
         public String Name { get; set; }
         public String Uri { get; set; }
-        public String[] Genres { get; set}
+        public String[] Genres { get; set; }
     }
 }
